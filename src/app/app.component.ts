1deb1ccf72d21ee0fbcbc77aee37ead662df6b0e
yyyydevtttt <yyyydevtttt@gmail.com>
2017-10-31 00:15:20 +0900

    addEventLitner を使うことにした

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,26 +12,27 @@ export class AppComponent implements OnInit {
 
   private db: IDBDatabase;
   private store: IDBObjectStore;
+  private indexedDB: IDBFactory;
   private dbName = 'sampleDB';
   private indexName = 'index';
   private storeName = 'sampleStore';
 
   ngOnInit() {
     // ベンタープレフィックス（webkit:Chrome、moz：FireFox）
-    const indexedDB = window.indexedDB;
+    this.indexedDB = window.indexedDB;
     // DB接続
-    this.db = null;
-    const openReq  = indexedDB.open(this.dbName);
+    let db = null;
+    const openReq  = this.indexedDB.open(this.dbName, 2);
 
     // 接続成功（初回 or DBのバージョンが変わった時）
-    openReq.onupgradeneeded = (event) => {
-      this.db = (<IDBRequest>event.target).result;
+    openReq.addEventListener('upgradeneeded', (event) => {
+      db = (<IDBRequest>event.target).result;
       console.log('db upgrade1');
 
       // オブジェクトストア作成
-      this.store = this.db.createObjectStore(this.storeName, { keyPath: 'id' });
+      this.store = db.createObjectStore('sampleStore', { keyPath: 'id' });
       // インデックスの作成
-      this.store.createIndex(this.indexName, 'id');
+      this.store.createIndex('index', 'id');
       console.log('db upgrade');
     // };
 
@@ -48,12 +49,19 @@ export class AppComponent implements OnInit {
       // データの登録・更新
       const putReq = this.store.put(data);
 
-    };
+      putReq.addEventListener('success', function(e) {
+        console.log('success!!');
+      });
+      putReq.addEventListener('error', function(e) {
+        console.log('error!!');
+      });
+
+    });
 
     // 接続に失敗
-    openReq.onerror = (event) => {
+    openReq.addEventListener('error', (event) => {
       console.log('db open error', event);
-    };
+    });
 
   }
 
@@ -61,18 +69,25 @@ export class AppComponent implements OnInit {
     this.inputData.push(form.value.input1);
 
     // DB接続
-    const openReq  = indexedDB.open(this.dbName, 1);
-    const db = this.db;
+    const openReq  = this.indexedDB.open(this.dbName, 2);
     const storeName = this.storeName;
 
-    openReq.onsuccess = function(event) {
+    openReq.addEventListener('success', function(event) {
+      const db = openReq.result;
+      console.log(db);
+      console.log('submit start');
       // トランザクションの準備
-      const store = db.transaction([storeName], 'readwrite').objectStore(storeName);
+      const trans = db.transaction(storeName, 'readwrite');
+      const store = trans.objectStore(storeName);
       const data = {id : form.value.input1, name : 'hoge'};
+
+      console.log('submit set up');
 
       // データの登録・更新
       // const putReq = store.put(data);
       const putReq = store.put(form.value.input1);
+
+      console.log('submit finish');
 
       // 登録・更新処理成功
       putReq.onsuccess = function(event_) {
@@ -82,7 +97,7 @@ export class AppComponent implements OnInit {
       putReq.onerror = function(event_) {
         console.log('登録失敗:' + event_);
       };
-    };
+    });
   }
 
   search() {
@@ -90,12 +105,18 @@ export class AppComponent implements OnInit {
     let result;
 
     // DB接続
-    const openReq  = indexedDB.open(this.dbName, 1);
+    const openReq  = this.indexedDB.open(this.dbName, 2);
 
-    openReq.onsuccess = (event) => {
+    openReq.addEventListener('success', (event) => {
+      const db = openReq.result;
       console.log('test 1');
       // トランザクションの準備
-      const store = this.db.transaction([this.storeName], 'readonly').objectStore(this.storeName);
+      const store = db   .transaction(this.storeName, 'readonly');
+      store.addEventListener('error', (e_error1) => {
+        // console.log(e_error1);
+      });
+
+      // .objectStore(this.storeName);
       console.log('test 2');
 
       // データ取得
@@ -108,7 +129,7 @@ export class AppComponent implements OnInit {
       } else {
         console.log('データはありません');
       }
-    };
+    });
 
   }
 }
